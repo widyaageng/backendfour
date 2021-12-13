826142398dd0535ac9e125c66c99883d3851cd60
widyaageng <ageng.setyatutuko@gmail.com>
2021-12-14 00:46:07 +0800

    Adding: 1. /api router to add user 2. /api router to add activity based on user id 3. /api router to delete all (dev)
    ./modules/db:
    1. createUser(username)
    2. addActivity(creatUser._id, activitylog({description, duration, date})
    3. deleteAll(cb)

--- server.js
+++ server.js
@@ -1,17 +1,88 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routerapi = express.Router();
+const Activity = require("./modules/db.js").ActivityModel;
+const TIMEOUT = 2000; // 2secs timeout
 
-app.use(cors())
-app.use(express.static('public'))
+// -------------------------------- custom middlewares --------------------------------
+//app logger
+function appLogger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+
+// -------------------------------- end of custom middlewares --------------------------------
+
+// -------------------- utility methods --------------------
+
+// -------------------- end of utility methods --------------------
+
+// -------------------- apply middlewares --------------------
+app.use(cors());
+app.use(express.static('public'));
+app.use(path = '/', middleWareFunction = appLogger);
+app.use(path = '/', middleWareFunction = bodyParser.urlencoded({ extended: true }));
+app.use(path = '/', middlewareFunction = bodyParser.json());
+app.use(path = '/api', routerapi);
+// -------------------- end of apply middlewares --------------------
+
+
+// -------------------- REST handler and router --------------------
+// get /root
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// /api/users
+const addUser = require("./modules/db").createUser;
+routerapi.post('/users', function (req, res, next) {
+  let t = setTimeout(() => {
+    next({ message: "timeout" });
+  }, TIMEOUT);
+
+  addUser(req.body['username'], function (err, data) {
+    clearTimeout(t);
+    if (err) return next(err);
+    res.json(data);
+    next();
+  });
+});
+
+// /api/users/:_id/exercises
+const addActivity = require("./modules/db").addActivityArray;
+routerapi.post('/users/:_id/exercises', function (req, res, next) {
+  // let t = setTimeout(() => {
+  //   next({ message: "timeout" });
+  // }, TIMEOUT);
+
+  addActivity(req.params['_id'], req.body, function (err, data) {
+    // clearTimeout(t);
+    console.log('Request body', req.body);
+    if (err) return next(err);
+    res.send(data);
+    next();
+  });
+});
+
+const deleteAll = require("./modules/db").deleteActivityPromise;
+routerapi.get('/deleteall', function (req, res, next) {
+  let t = setTimeout(() => {
+    next({ message: "timeout" });
+  }, TIMEOUT);
+
+  deleteAll(function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+    next();
+  })
+
 });
 
 
-
-
+// -------------------- end of REST handler and router --------------------
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
