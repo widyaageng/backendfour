4b1542868f808d93aa967bf22d3dec72aa89ba82
widyaageng <ageng.setyatutuko@gmail.com>
2021-12-16 22:40:29 +0800

    Splitting models for easier implementation on conditional query on field data

--- modules/db.js
+++ modules/db.js
@@ -9,18 +9,21 @@ mongoose.connect(uri, {
 
 const { Schema } = mongoose;
 
+const exerciseSchema = new Schema({
+    description: String,
+    duration: Number,
+    date: String
+})
+
 const activitySchema = new Schema({
     username: String,
     count: Number,
     _id: String,
-    log: [{
-        description: String,
-        duration: Number,
-        date: String
-    }]
+    log: [exerciseSchema]
 });
 
 let ActivityModel = mongoose.model('Activity', activitySchema);
+let ExerciseModel = mongoose.model('Exercise', exerciseSchema);
 
 const logKeys = ['description', 'duration', 'date'];
 const typeValues = ['String', 'Number', 'String'];
@@ -57,14 +60,14 @@ const checkActivityLog = (activityLog) => {
 //-------------------- Database function --------------------
 // Create User
 const createUser = (user, done) => {
-    let acivityEntry = {
+    let activityEntry = {
         username: user,
         count: 0,
         _id: generateId(),
         log: []
     }
 
-    let newActivity = new ActivityModel(acivityEntry);
+    let newActivity = new ActivityModel(activityEntry);
 
     newActivity.save(function (err, data) {
         if (err) return done(err, null);
@@ -74,36 +77,74 @@ const createUser = (user, done) => {
 
 // Add activity: function(need user_id)
 const addActivityArray = (id, activityJSON, done) => {
-    ActivityModel.findOneAndUpdate(
-        { _id: id },
-        { $push: { log: activityJSON } },
-        {new: true},
-        function (err, data) {
-            if (err) return done(err, null);
-            if (data == null) return done(new Error("Data is null!"), null);
-            
-            let reformattedDate = new Date(data['log'].slice(-1)[0]['date']);
-            
-            //This will generate ['Www', 'dd', 'Mmm', 'yyyy'] array
-            reformattedDate = reformattedDate.toUTCString().split(' ').slice(0,4);
-            //Reformat to be Www Mmm dd yyyy
-            let formattedStringDate = `${reformattedDate[0].slice(0,-1)} ${reformattedDate[2]} ${reformattedDate[1]} ${reformattedDate[3]}`;
-            console.log("Formatted string date", formattedStringDate);
+    checkActivityLog(activityJSON);
 
-            done(null, [...data['log'], {
-                username: data['username'],
-                description: data['log'].slice(-1)[0]['description'],
-                duration: parseInt(data['log'].slice(-1)[0]['duration']),
-                date: formattedStringDate,
-                _id: data['log'].slice(-1)[0]['_id']
-            }]);
-        }
-    )
+    let newExercise = new ExerciseModel(activityJSON);
+    newExercise.save(function (err, data) {
+        if (err) return done(err, null);
+        ActivityModel.findOneAndUpdate(
+            { _id: id },
+            { $push: { log: newExercise } },
+            { new: true },
+            function (err, data) {
+                if (err) return done(err, null);
+                if (data == null) return done(new Error("Data is null!"), null);
+
+                let reformattedDate = new Date(data['log'].slice(-1)[0]['date']);
+
+                //This will generate ['Www', 'dd', 'Mmm', 'yyyy'] array
+                reformattedDate = reformattedDate.toUTCString().split(' ').slice(0, 4);
+                //Reformat to be Www Mmm dd yyyy
+                let formattedStringDate = `${reformattedDate[0].slice(0, -1)} ${reformattedDate[2]} ${reformattedDate[1]} ${reformattedDate[3]}`;
+                
+                done(null, [...data['log'], {
+                    username: data['username'],
+                    description: data['log'].slice(-1)[0]['description'],
+                    duration: parseInt(data['log'].slice(-1)[0]['duration']),
+                    date: formattedStringDate,
+                    _id: data['log'].slice(-1)[0]['_id']
+                }]);
+            }
+        );
+    })
+    // ExerciseModel
+    // ActivityModel.findOneAndUpdate(
+    //     { _id: id },
+    //     { $push: { log: activityJSON } },
+    //     {new: true},
+    //     function (err, data) {
+    //         if (err) return done(err, null);
+    //         if (data == null) return done(new Error("Data is null!"), null);
+
+    //         let reformattedDate = new Date(data['log'].slice(-1)[0]['date']);
+
+    //         //This will generate ['Www', 'dd', 'Mmm', 'yyyy'] array
+    //         reformattedDate = reformattedDate.toUTCString().split(' ').slice(0,4);
+    //         //Reformat to be Www Mmm dd yyyy
+    //         let formattedStringDate = `${reformattedDate[0].slice(0,-1)} ${reformattedDate[2]} ${reformattedDate[1]} ${reformattedDate[3]}`;
+
+    //         done(null, [...data['log'], {
+    //             username: data['username'],
+    //             description: data['log'].slice(-1)[0]['description'],
+    //             duration: parseInt(data['log'].slice(-1)[0]['duration']),
+    //             date: formattedStringDate,
+    //             _id: data['log'].slice(-1)[0]['_id']
+    //         }]);
+    //     }
+    // )
 };
 // Find activity with time range
-// Delete all documents
+// Delete all activity documents
 const deleteActivityPromise = (done) => {
     ActivityModel.deleteMany({}, function (err, data) {
+        if (err) return done(err, null);
+        done(null, data);
+    });
+}
+
+// Delete all exercise documents
+const deleteExcercisePromise = (done) => {
+    ExerciseModel.deleteMany({}, function (err, data) {
         if (err) return done(err, null);
         done(null, data);
     });
@@ -116,3 +157,4 @@ exports.ActivityModel = ActivityModel;
 exports.createUser = createUser;
 exports.addActivityArray = addActivityArray;
 exports.deleteActivityPromise = deleteActivityPromise;
+exports.deleteExcercisePromise = deleteExcercisePromise;
